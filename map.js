021e06f7d5c7ef779858b40f88ffe93aa387a40d
sudharsan7150 <126057669+sudharsan7150@users.noreply.github.com>
2023-04-04 19:36:31 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,17 @@
+var map1 = new Map([
+	[1, 2],
+	[2, 3],
+	[4, 5]
+]);
+
+console.log("Map1");
+console.log(map1);
+
+var map2 = new Map([
+	["firstname", "sudharsan"],
+	["lastname", "sudharsan"],
+	["website", "my web"]
+]);
+
+console.log("Map2");
+console.log(map2);
